49de1cfc1fb2c10a9cda0d7548975f4b4b1bd4cf
Ryan Braun <ryan.m.braun@hotmail.com>
2023-05-09 03:45:39 -0400

    styled modules updates

--- tests/nextjs/src/app/test/button.tsx
+++ tests/nextjs/src/app/test/button.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import styles from './button.module.css'
+import styles from './button.module.scss'
 import listen from '@rydux/listener'
 
 import testReducer from '@rydux/test.reducer'
@@ -8,34 +8,38 @@ import { FullStore } from '@rydux/rydux'
 
 import styled from '@styled'
 import React, { CSSProperties, useRef } from 'react'
+import css from 'styled-jsx/css'
 
-const MyButton = styled('button', styles.buttonTest, (props: { deg: number; buttonId: number }) => ({
-  padding: '10px',
-  border: '2px solid white',
-  background: props.buttonId % 2 === 0 ? `hsl(${props.deg.toString()} 100% 50%)` : 'blue',
+const MyButton = styled.button(styles.buttonTest, (props: { deg: number; buttonId: number }) => ({
+  background:
+    props.buttonId % 2 === 0
+      ? `hsl(${props.deg.toString()} 100% 50%)`
+      : `hsl(${(props.deg * 1.5).toString()} 100% 50%)`,
+  _position: 'relative',
 }))
 
-const IdAndCountText = styled('p', styles.textTest, (props: { test: string; fontSize: string }) => ({
+const IdAndCountText = styled.p(styles.textTest, (props: { test: string; fontSize: string }) => ({
   color: props.test,
   fontSize: props.fontSize,
   textDecoration: 'underline',
 }))
 
-const PlainText = styled('p')
+const PlainText = styled.p()
 
-const InheritText = styled('p', IdAndCountText, (props: { test2: string }) => ({
+const InheritText = styled.p(IdAndCountText, (props: { test2: string }) => ({
   color: props.test2,
 }))
 
-const TestNoProps = styled('p')
+const TestNoProps = styled.p()
 
-const InheritText2 = styled('p', PlainText, (props: { fontStyle: string }) => ({
+const InheritText2 = styled.p(PlainText, (props: { fontStyle: string }) => ({
   fontStyle: props.fontStyle,
+  _color: 'darkred',
 }))
 
-const InheritText3 = styled('p', InheritText)
+const InheritText3 = styled.p(InheritText)
 
-const InheritText4 = styled('p', [
+const InheritText4 = styled.p([
   styles.buttonTest,
   styles.textTest,
   styles.textTest,
@@ -46,7 +50,26 @@ const InheritText4 = styled('p', [
 
 const TestTest = styled.p(InheritText, (props: { test3: CSSProperties['color'] }) => ({
   color: props.test3,
+  position: 'relative',
 }))
+
+const FartTag = styled.fart(styles.textTest)
+
+const AnimatedText = styled.p(styles.text, (props: { speed: number }) => ({
+  animation: `${styles.textAnimation} ${500 * (props.speed ?? 1)}ms ease infinite`,
+  textDecoration: 'underline',
+}))
+
+const VariableText = styled.p(styles.variableText, (props: { color: CSSProperties['color'] }) => ({
+  color: props.color,
+  poop: 'test',
+}))
+
+console.log(css`
+  height: 4px;
+  width: 10px;
+  color: blue;
+`)
 
 export default listen<{ id: number }, { count: FullStore['test'][number] }>(
   (store, { id }) => ({ count: store.test[id] }),
@@ -113,6 +136,22 @@ export default listen<{ id: number }, { count: FullStore['test'][number] }>(
           {'InheritText3'}
         </InheritText3>
         <InheritText4>{'InheritText4'}</InheritText4>
+        <FartTag>{'Fart Tag'}</FartTag>
+        <AnimatedText
+          css={{
+            speed: id % 10,
+          }}
+        >
+          {'ANIMATED'}
+        </AnimatedText>
+        <VariableText
+          css={{
+            color: 'yellow',
+          }}
+        >
+          {'this is some variable text'}
+        </VariableText>
+        <VariableText css={{ color: 'turquoise' }}>{'this is MORE variable text'}</VariableText>
       </MyButton>
     )
   }
--- tests/nextjs/src/styled-css-modules/index.ts
+++ tests/nextjs/src/styled-css-modules/index.ts
@@ -1,133 +1,41 @@
-import React, { useMemo } from 'react'
+import React, { CSSProperties } from 'react'
+
+type CSSPropertyKeys = keyof React.CSSProperties
+
+type RawCSSProperties = {
+  [K in `_${CSSPropertyKeys}`]?: K extends `_${infer CssName}`
+    ? CssName extends CSSPropertyKeys
+      ? CSSProperties[CssName]
+      : never
+    : never
+}
 
 type Props = Record<string, unknown>
 
-type StyledFunction<P = Props> = (props: P) => React.CSSProperties | undefined
+type StyledFunction<P = Props> = (props: P) => (CSSProperties & RawCSSProperties & Record<string, unknown>) | undefined
 
 type TagNames = keyof JSX.IntrinsicElements
 
-type StyledComponent<Tag extends TagNames, P extends { css?: Props }> = React.FC<P & JSX.IntrinsicElements[Tag]> & {
+type CustomTag = string
+
+type BasicElementAttributes = { id?: string; children?: React.ReactNode }
+
+type StyledComponent<Tag extends CustomTag | TagNames, P extends { css?: Props }> = React.FC<
+  P & (Tag extends TagNames ? JSX.IntrinsicElements[Tag] : BasicElementAttributes)
+> & {
   className: string | undefined
   styleFunction: StyledFunction<any>
 }
 
-function styled<Tag extends TagNames>(
-  tagName: Tag,
-  extendsFrom?: null | undefined,
-  styleFunction?: null | undefined
-): StyledComponent<Tag, { css?: undefined }>
+type Styled<Tag extends string = CustomTag> = {
+  (): StyledComponent<Tag, {}>
 
-function styled<Tag extends TagNames>(
-  tagName: Tag,
-  extendsFrom: string | Array<string | null | undefined>,
-  styleFunction?: null | undefined
-): StyledComponent<Tag, { css?: undefined }>
+  (extendsFrom: string | Array<string | null | undefined>): StyledComponent<Tag, { css?: undefined }>
 
-function styled<Tag extends TagNames, ExtendsCssProps extends { css?: Props }>(
-  tagName: Tag,
-  extendsFrom: StyledComponent<any, ExtendsCssProps>,
-  styleFunction?: null | undefined
-): StyledComponent<Tag, ExtendsCssProps['css'] extends undefined ? {} : ExtendsCssProps>
-
-function styled<Tag extends TagNames, CssProps extends Props>(
-  tagName: Tag,
-  extendsFrom: string | Array<string | null | undefined>,
-  styleFunction: StyledFunction<CssProps>
-): StyledComponent<Tag, { css: CssProps }>
-
-function styled<Tag extends TagNames, CssProps extends Props, ExtendsCssProps extends { css?: Props }>(
-  tagName: Tag,
-  extendsFrom: StyledComponent<any, ExtendsCssProps>,
-  styleFunction: StyledFunction<CssProps>
-): StyledComponent<
-  Tag,
-  ExtendsCssProps['css'] extends undefined ? { css: CssProps } : { css: CssProps & ExtendsCssProps['css'] }
->
-
-function styled<
-  Tag extends TagNames,
-  CssProps extends Props,
-  ExtendsCssProps extends { css?: Props },
-  ReturnElement extends StyledComponent<
+  <ExtendsCssProps extends { css?: Props }>(extendsFrom: StyledComponent<any, ExtendsCssProps>): StyledComponent<
     Tag,
-    ExtendsCssProps['css'] extends undefined ? { css: CssProps } : { css: CssProps & ExtendsCssProps['css'] }
+    ExtendsCssProps['css'] extends undefined ? {} : ExtendsCssProps
   >
->(
-  tagName: Tag,
-  extendsFrom?: string | StyledComponent<any, ExtendsCssProps> | Array<string | null | undefined> | null | undefined,
-  styleFunction?: StyledFunction<CssProps> | null | undefined
-): ReturnElement {
-  if (typeof tagName !== 'string') {
-    throw new Error('tagName must be a valid JSX element tag name')
-  }
-
-  if (
-    extendsFrom != null &&
-    typeof extendsFrom !== 'string' &&
-    !(Array.isArray(extendsFrom) && extendsFrom.every((value) => value == null || typeof value === 'string')) &&
-    !('styleFunction' in extendsFrom)
-  ) {
-    throw new Error('extendsFrom must be a String, StyledComponent, or Array<String>')
-  }
-
-  if (styleFunction != null && typeof styleFunction !== 'function') {
-    throw new Error('styleFunction must be a function that returns React.CSSProperties')
-  }
-
-  const parentClassName =
-    extendsFrom == null
-      ? undefined
-      : typeof extendsFrom === 'string'
-      ? extendsFrom.trim()
-      : Array.isArray(extendsFrom)
-      ? Array.from(
-          new Set(
-            extendsFrom.flatMap((value) => value?.split(' ')?.map((value) => value.trim())).filter((value) => !!value)
-          )
-        ).join(' ')
-      : extendsFrom.className
-
-  const parentStyleFunction =
-    extendsFrom == null ||
-    typeof extendsFrom === 'string' ||
-    Array.isArray(extendsFrom) ||
-    extendsFrom.styleFunction == null
-      ? null
-      : extendsFrom.styleFunction
-
-  const mergedStyleFunction = (css?: CssProps) =>
-    css == null ? undefined : Object.assign({}, parentStyleFunction?.(css) ?? {}, styleFunction?.(css) ?? {})
-
-  const component = React.forwardRef(function Styled(
-    props: { css?: CssProps; className?: string; style: React.CSSProperties },
-    ref
-  ) {
-    return React.createElement(tagName, {
-      ...props,
-      ref,
-      className: useMemo(() => `${props.className || ''} ${parentClassName || ''}`.trim(), [props.className]),
-      style: useMemo(() => mergedStyleFunction(props.css), [props.css]),
-    })
-  }) as unknown as ReturnElement
-
-  component.className = parentClassName
-  component.styleFunction = mergedStyleFunction
-
-  return component
-}
-
-type Styled<Tag extends TagNames> = {
-  (extendsFrom?: null | undefined, styleFunction?: null | undefined): StyledComponent<Tag, { css?: undefined }>
-
-  <CssProps extends Props>(
-    extendsFrom: string | Array<string | null | undefined>,
-    styleFunction?: null | undefined
-  ): StyledComponent<Tag, { css?: undefined }>
-
-  <CssProps extends Props, ExtendsCssProps extends { css?: Props }>(
-    extendsFrom: StyledComponent<any, ExtendsCssProps>,
-    styleFunction?: null | undefined
-  ): StyledComponent<Tag, ExtendsCssProps['css'] extends undefined ? {} : ExtendsCssProps>
 
   <CssProps extends Props>(
     extendsFrom: string | Array<string | null | undefined>,
@@ -151,12 +59,85 @@ type Styled<Tag extends TagNames> = {
   >
 }
 
-const wrappedStyled = new Proxy(styled, {
-  get(target, key) {
-    return (...args: any[]) => target(key as TagNames, ...args)
-  },
-}) as typeof styled & {
-  [K in keyof JSX.IntrinsicElements]: Styled<K>
-}
+export default new Proxy(
+  function (
+    tagName: string,
+    extendsFrom?: string | StyledComponent<any, any> | Array<string | null | undefined> | null | undefined,
+    styleFunction?: StyledFunction<any> | null | undefined
+  ) {
+    if (typeof tagName !== 'string') {
+      throw new Error('tagName must be a valid JSX element tag name')
+    }
 
-export default wrappedStyled
+    if (
+      extendsFrom != null &&
+      typeof extendsFrom !== 'string' &&
+      !(Array.isArray(extendsFrom) && extendsFrom.every((value) => value == null || typeof value === 'string')) &&
+      !('styleFunction' in extendsFrom)
+    ) {
+      throw new Error('extendsFrom must be a String, StyledComponent, or Array<String>')
+    }
+
+    if (styleFunction != null && typeof styleFunction !== 'function') {
+      throw new Error('styleFunction must be a function that returns CSSProperties')
+    }
+
+    const parentClassName =
+      extendsFrom == null
+        ? undefined
+        : typeof extendsFrom === 'string'
+        ? extendsFrom.trim()
+        : Array.isArray(extendsFrom)
+        ? Array.from(
+            new Set(
+              extendsFrom.flatMap((value) => value?.split(' ')?.map((value) => value.trim())).filter((value) => !!value)
+            )
+          ).join(' ')
+        : extendsFrom.className
+
+    const parentStyleFunction =
+      extendsFrom == null ||
+      typeof extendsFrom === 'string' ||
+      Array.isArray(extendsFrom) ||
+      extendsFrom.styleFunction == null
+        ? null
+        : extendsFrom.styleFunction
+
+    const mergedStyleFunction = (css: Props = {}) =>
+      Object.assign({}, parentStyleFunction?.(css) ?? {}, styleFunction?.(css) ?? {})
+
+    const component = React.memo(
+      React.forwardRef(function Styled(props: { css?: Props; className?: string; style: CSSProperties }, ref) {
+        const { css, className, style, ...rest } = props
+
+        return React.createElement(tagName, {
+          ...rest,
+          ref,
+          className: React.useMemo(() => `${className || ''} ${parentClassName || ''}`.trim(), [className]),
+          style: React.useMemo(
+            () =>
+              Object.fromEntries(
+                Object.entries(mergedStyleFunction(css)).map(([key, value]) => [
+                  key.startsWith('_') ? key.slice(1) : `--${key}`,
+                  value,
+                ])
+              ),
+            [css]
+          ),
+        })
+      })
+    ) as unknown as StyledComponent<string, any>
+
+    component.className = parentClassName
+    component.styleFunction = mergedStyleFunction
+
+    return component
+  },
+  {
+    get(target, key) {
+      return (...args: any[]) => target(key.toString(), ...args)
+    },
+  }
+) as unknown as {
+  [K in keyof JSX.IntrinsicElements]: Styled<K>
+} & { [K: string]: Styled }
